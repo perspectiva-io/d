94d7cea8e6cba6e7e4e7ce1e542becce6758ad76
Juan Lulkin <maiz@lulk.in>
2014-06-26 20:50:09 +0300

    Add edit and update w json to controller

--- app/controllers/debates_controller.rb
+++ app/controllers/debates_controller.rb
@@ -3,10 +3,16 @@ class DebatesController < ApplicationController
   class NotFound < StandardError; end
   class DebateNotFound < NotFound; end
 
+  respond_to :json
+
   rescue_from NotFound, with: :not_found
 
   def new
-    @debate = Debate.new
+    redirect_to edit_debate_path(Debate.create)
+  end
+
+  def edit
+    @debate = Debate[params[:id]]
   end
 
   def not_found
@@ -19,5 +25,9 @@ class DebatesController < ApplicationController
 
   def show
     @debate = Debate[params[:id]] or raise DebateNotFound
+  end
+
+  def update
+    respond_with Debate[params[:id]].update params[:field] => params[:value]
   end
 end
